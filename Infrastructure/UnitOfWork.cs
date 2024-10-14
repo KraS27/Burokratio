91db26c133571f5a1fb7b15938bb37a89192bc44
KraSS <groverkras@gmail.com>
2024-10-14 15:12:01 +0300

    Add UOW

---
+++ Infrastructure/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+
+namespace Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
