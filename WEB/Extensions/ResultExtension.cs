e009c01f0fa908b200ddc82ee206d83c93f0a0b8
KraSS <groverkras@gmail.com>
2024-10-16 19:19:14 +0300

    Improve ToProblemDetails method in ResultExtension class

--- WEB/Extensions/ResultExtension.cs
+++ WEB/Extensions/ResultExtension.cs
@@ -11,8 +11,8 @@ namespace WEB.Extensions
 
             return Results.Problem(
                 statusCode: GetStatusCode(result.Error.ErrorType),
-                title: result.Error.ErrorType.ToString(),
-                type: "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                title: GetTitle(result.Error.ErrorType),
+                type: GetType(result.Error.ErrorType),
                 extensions: new Dictionary<string, object?>
                 {
                     {"errors", new[] {result.Error} }
@@ -20,13 +20,31 @@ namespace WEB.Extensions
 
 
             static int GetStatusCode(ErrorType errorType) =>
-            errorType switch
-            {
-                ErrorType.Validation => StatusCodes.Status400BadRequest,
-                ErrorType.NotFound => StatusCodes.Status404NotFound,
-                ErrorType.Conflict => StatusCodes.Status409Conflict,
-                _ => StatusCodes.Status500InternalServerError
-            };
+                errorType switch
+                {
+                    ErrorType.Validation => StatusCodes.Status400BadRequest,
+                    ErrorType.NotFound => StatusCodes.Status404NotFound,
+                    ErrorType.Conflict => StatusCodes.Status409Conflict,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+            static string GetTitle(ErrorType errorType) =>
+                errorType switch
+                {
+                    ErrorType.Validation => "Bad Request",
+                    ErrorType.NotFound => "Not Found",
+                    ErrorType.Conflict => "Conflict",
+                    _ => "Server Failure"
+                };
+
+            static string GetType(ErrorType statusCode) =>
+                statusCode switch
+                {
+                    ErrorType.Validation => "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                    ErrorType.NotFound => "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                    ErrorType.Conflict => "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+                    _ => "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+                };
         }                
     }
 }
